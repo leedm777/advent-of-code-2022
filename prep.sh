ebbea3ecffaf9522746988c0904b44366aadcde6
David M. Lee <leedm777@yahoo.com>
2022-12-01 20:46:46 -0600

    Fix stub in prep.sh

--- prep.sh
+++ prep.sh
@@ -12,8 +12,8 @@ year=$(date +%Y)
 day=$(date +%d)
 input=./src/day${day}.txt
 
-if test -e src/day${day}.rs; then
-  echo "day${day}.rs already exists" >&2
+if test -e src/day${day}.js; then
+  echo "day${day}.js already exists" >&2
   exit 1
 fi
 
@@ -27,7 +27,43 @@ curl \
 head ${input}
 
 cat <<EOF > src/day${day}.js
+import _ from "lodash";
+
+export function part1(input) {
+}
+
+export function part2(input) {
 EOF
 
 cat <<EOF > src/day${day}.spec.js
+import { part1, part2 } from "./day${day}";
+import { readInput } from "./aoc";
+
+const puzzleInput = readInput("./src/day${day}.txt");
+const exampleInput = [
+];
+
+describe("day${day}", () => {
+  describe("part 1", () => {
+    it("should work with the sample", () => {
+      const actual = part1(exampleInput);
+      expect(actual).toStrictEqual();
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part1(puzzleInput);
+      expect(actual).toStrictEqual();
+    });
+  });
+
+  describe("part 2", () => {
+    it("should work with the sample", () => {
+      const actual = part2(exampleInput);
+      expect(actual).toStrictEqual();
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part2(puzzleInput);
+      expect(actual).toStrictEqual();
+    });
+  });
+});
 EOF

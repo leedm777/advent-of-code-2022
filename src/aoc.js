428e9b8b99cd79a86f605b879b35e88e22a6dbab
David M. Lee <leedm777@yahoo.com>
2022-12-01 21:37:17 -0600

    Add a runner for all the solutions

--- src/aoc.js
+++ src/aoc.js
@@ -1,5 +1,5 @@
 import fs from "fs";
 
 export function readInput(filename) {
-  return fs.readFileSync("./src/day01.txt", "utf-8").split("\n");
+  return fs.readFileSync(filename, "utf-8").split("\n");
 }
---
+++ src/main.js
@@ -0,0 +1,33 @@
+import _ from 'lodash';
+import {readInput} from "./aoc.js";
+
+async function main() {
+  return _.range(1, 26)
+    .reduce(async (prior, day) => {
+      await prior;
+
+      try {
+        let dayStr = _.padStart(day.toString(10), 2, '0');
+        const puzzleInput = readInput(`./src/day${dayStr}.txt`);
+        const solver = await import(`./day${dayStr}.js`);
+
+        console.log(`Day ${day}:`)
+
+        const soln1 = solver.part1(puzzleInput);
+        console.log(`  part1: ${soln1}`);
+
+        const soln2 = solver.part2(puzzleInput);
+        console.log(`  part2: ${soln2}`);
+      } catch (err) {
+        // ENOENT means the input file is missing; skip
+        if (err.code === 'ENOENT') {
+          return;
+        }
+        throw err;
+      }
+    }, Promise.resolve())
+}
+
+main().catch(err => process.nextTick(() => {
+  throw err;
+}))

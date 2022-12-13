728914f1901ac00e3f634919cf7ac9912b4a94ee
David M. Lee <leedm777@yahoo.com>
2022-12-13 09:44:59 -0600

    Day 13, part 1

---
+++ src/day13.js
@@ -0,0 +1,60 @@
+import _ from "lodash";
+import assert from "assert";
+
+export function compare(lhs, rhs) {
+  // console.log(JSON.stringify({ lhs, rhs }));
+  if (_.isNumber(lhs) && _.isNumber(rhs)) {
+    return Math.sign(lhs - rhs);
+  }
+
+  if (_.isNil(lhs) && _.isNil(rhs)) {
+    return 0;
+  }
+  if (_.isNil(lhs) && !_.isNil(rhs)) {
+    return -1;
+  }
+  if (!_.isNil(lhs) && _.isNil(rhs)) {
+    return 1;
+  }
+
+  if (_.isArray(lhs) && _.isArray(rhs)) {
+    if (_.isEmpty(lhs) && _.isEmpty(rhs)) {
+      return 0;
+    }
+
+    const [lhsFirst, ...lhsRest] = lhs;
+    const [rhsFirst, ...rhsRest] = rhs;
+
+    const cmpFirst = compare(lhsFirst, rhsFirst);
+    if (cmpFirst !== 0) {
+      return cmpFirst;
+    }
+
+    return compare(lhsRest, rhsRest);
+  }
+
+  if (_.isArray(lhs) && _.isNumber(rhs)) {
+    return compare(lhs, [rhs]);
+  }
+
+  if (_.isNumber(lhs) && _.isArray(rhs)) {
+    return compare([lhs], rhs);
+  }
+
+  assert.fail(`Unknown comparison ${JSON.stringify({ lhs, rhs })}`);
+}
+export function part1(input) {
+  return _(input)
+    .chunk(3)
+    .map(([lhsStr, rhsStr]) => ({
+      lhs: JSON.parse(lhsStr),
+      rhs: JSON.parse(rhsStr),
+    }))
+    .map(({ lhs, rhs }) => compare(lhs, rhs))
+    .map((cmp, idx) => (cmp === -1 ? idx + 1 : 0))
+    .sum();
+}
+
+export function part2(input) {
+  return input;
+}
---
+++ src/day13.spec.js
@@ -0,0 +1,53 @@
+import { part1, part2 } from "./day13";
+import { readInput } from "./aoc";
+
+const puzzleInput = readInput("./src/day13.txt");
+const exampleInput = [
+  "[1,1,3,1,1]",
+  "[1,1,5,1,1]",
+  "",
+  "[[1],[2,3,4]]",
+  "[[1],4]",
+  "",
+  "[9]",
+  "[[8,7,6]]",
+  "",
+  "[[4,4],4,4]",
+  "[[4,4],4,4,4]",
+  "",
+  "[7,7,7,7]",
+  "[7,7,7]",
+  "",
+  "[]",
+  "[3]",
+  "",
+  "[[[]]]",
+  "[[]]",
+  "",
+  "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+  "[1,[2,[3,[4,[5,6,0]]]],8,9]",
+];
+
+describe("day13", () => {
+  describe("part 1", () => {
+    it("should work with the sample", () => {
+      const actual = part1(exampleInput);
+      expect(actual).toStrictEqual(13);
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part1(puzzleInput);
+      expect(actual).toStrictEqual(5292);
+    });
+  });
+
+  describe("part 2", () => {
+    it("should work with the sample", () => {
+      const actual = part2(exampleInput);
+      expect(actual).toStrictEqual();
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part2(puzzleInput);
+      expect(actual).toStrictEqual();
+    });
+  });
+});

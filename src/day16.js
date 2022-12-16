fcb9d9e4f8000735c11719fc985aad3c35f124de
David M. Lee <leedm777@yahoo.com>
2022-12-16 07:04:02 -0600

    Day 16, part 1, but gives the wrong answer :-/

---
+++ src/day16.js
@@ -0,0 +1,109 @@
+import _ from "lodash";
+import { dijkstraHeuristic, findPath } from "./aoc.js";
+import assert from "assert";
+
+function parseLine(line) {
+  const re =
+    /Valve (?<valveName>[A-Z][A-Z]) has flow rate=(?<flowRate>\d+); tunnels? leads? to valves? (?<leadTo>.*)*/;
+  const { groups } = line.match(re);
+
+  return {
+    valveName: groups.valveName,
+    flowRate: parseInt(groups.flowRate, 10),
+    leadTo: _.split(groups.leadTo, ", "),
+  };
+}
+
+class Valves {
+  constructor(valves) {
+    this.valves = valves;
+    this.start = {
+      valveName: "AA",
+      // assume any flowRate of 0 is already open
+      closedValves: _(valves).filter("flowRate").map("valveName").value(),
+    };
+    this.h = dijkstraHeuristic;
+  }
+
+  isGoal(n) {
+    return _.isEmpty(n.closedValves);
+  }
+
+  getNeighbors({ valveName, closedValves }) {
+    const v = this.valves[valveName];
+    assert.ok(v, `Could not find valve ${valveName}`);
+    const r = _.map(v.leadTo, (nextValveName) => ({
+      valveName: nextValveName,
+      closedValves,
+    }));
+
+    // If this valve is open, close it
+    const i = _.indexOf(closedValves, valveName);
+    if (i >= 0) {
+      const nextClosedValves = _.clone(closedValves);
+      nextClosedValves.splice(i, 1);
+      r.push({
+        valveName,
+        closedValves: nextClosedValves,
+      });
+    }
+
+    return r;
+  }
+
+  getNeighborDistance(current, neighbor) {
+    // opportunity cost of the closed valves
+    const oppCost = _(neighbor.closedValves)
+      .map((valveName) => {
+        const valve = this.valves[valveName];
+        assert.ok(valve, `Could not find valve named ${valveName}`);
+        return valve;
+      })
+      .map("flowRate")
+      .sum();
+
+    // add 1 for either moving tunnels or opening the valves
+    return oppCost + 1;
+  }
+
+  keyify({ valveName, closedValves }) {
+    const s = _.sortBy(closedValves);
+    s.unshift(valveName);
+    return _.join(s, ",");
+  }
+}
+
+export function part1(input) {
+  const valves = new Valves(_(input).map(parseLine).keyBy("valveName").value());
+  const path = findPath(valves);
+
+  let log = "";
+
+  const { totalPressure } = _.reduce(
+    _.zip(path, _.range(0, 30)),
+    ({ totalPressure }, [node, minute]) => {
+      const closedValves = _.get(node, "closedValves", []);
+      const thisPressure = _(valves.valves)
+        .filter(({ valveName }) => !_.includes(closedValves, valveName))
+        .map("flowRate")
+        .sum();
+
+      log = `${log}\n${minute + 1}: ${thisPressure}`;
+      return {
+        totalPressure: totalPressure + thisPressure,
+      };
+    },
+    {
+      totalPressure: 0,
+    }
+  );
+
+  console.log(log);
+
+  return totalPressure;
+}
+
+export function part2(input) {
+  const valves = new Valves(_(input).map(parseLine).keyBy("valveName").value());
+  return valves;
+}
---
+++ src/day16.spec.js
@@ -0,0 +1,40 @@
+import { part1, part2 } from "./day16";
+import { readInput } from "./aoc";
+
+const puzzleInput = readInput("./src/day16.txt");
+const exampleInput = [
+  "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB",
+  "Valve BB has flow rate=13; tunnels lead to valves CC, AA",
+  "Valve CC has flow rate=2; tunnels lead to valves DD, BB",
+  "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE",
+  "Valve EE has flow rate=3; tunnels lead to valves FF, DD",
+  "Valve FF has flow rate=0; tunnels lead to valves EE, GG",
+  "Valve GG has flow rate=0; tunnels lead to valves FF, HH",
+  "Valve HH has flow rate=22; tunnel leads to valve GG",
+  "Valve II has flow rate=0; tunnels lead to valves AA, JJ",
+  "Valve JJ has flow rate=21; tunnel leads to valve II",
+];
+
+describe("day16", () => {
+  describe("part 1", () => {
+    it("should work with the sample", () => {
+      const actual = part1(exampleInput);
+      expect(actual).toStrictEqual(1651);
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part1(puzzleInput);
+      expect(actual).toStrictEqual();
+    });
+  });
+
+  describe("part 2", () => {
+    it("should work with the sample", () => {
+      const actual = part2(exampleInput);
+      expect(actual).toStrictEqual();
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part2(puzzleInput);
+      expect(actual).toStrictEqual();
+    });
+  });
+});

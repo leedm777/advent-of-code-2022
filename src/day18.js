768a055a86dc7379d227fb7ab612c61d8ebe5ffb
David M. Lee <leedm777@yahoo.com>
2022-12-21 06:33:33 -0600

    Day 18, part 2 WIP

--- src/day18.js
+++ src/day18.js
@@ -18,27 +18,28 @@ function getNeighborCell(cell, direction) {
   return _(cell).zip(direction).map(_.sum).value();
 }
 
+function setCell(grid, cell) {
+  // eslint-disable-next-line lodash/path-style
+  const self = {
+    numNeighbors: 0,
+    steamed: 0,
+  };
+  _.set(grid, cell, self);
+
+  for (const d of Directions) {
+    const neighborCell = getNeighborCell(cell, d);
+    const neighbor = _.get(grid, neighborCell);
+    if (neighbor) {
+      ++neighbor.numNeighbors;
+      ++self.numNeighbors;
+    }
+  }
+
+  return grid;
+}
+
 export function part1(input) {
-  const grid = _(input)
-    .map(parseLine)
-    .reduce((grid, cell) => {
-      // eslint-disable-next-line lodash/path-style
-      const self = {
-        numNeighbors: 0,
-      };
-      _.set(grid, cell, self);
-
-      for (const d of Directions) {
-        const neighborCell = getNeighborCell(cell, d);
-        const neighbor = _.get(grid, neighborCell);
-        if (neighbor) {
-          ++neighbor.numNeighbors;
-          ++self.numNeighbors;
-        }
-      }
-
-      return grid;
-    }, []);
+  const grid = _(input).map(parseLine).reduce(setCell, []);
 
   return _(grid)
     .flatten()
@@ -49,6 +50,52 @@ export function part1(input) {
 }
 
 export function part2(input) {
-  // TODO
-  return _.map(input);
+  const cells = _.map(input, parseLine);
+  const grid = _.reduce(cells, setCell, []);
+  const limits = _.reduce(
+    cells,
+    (maxes, cell) => _(maxes).zip(cell).map(_.max).value(),
+    [-1, -1, -1]
+  );
+
+  const open = [[0, 0, 0]];
+  const visited = new Set();
+
+  let current;
+  while ((current = open.pop())) {
+    if (visited.has(JSON.stringify(current))) {
+      continue;
+    }
+    visited.add(JSON.stringify(current));
+
+    for (const d of Directions) {
+      const cell = getNeighborCell(current, d);
+
+      // skip if we are off the bottom of the grid
+      if (_.some(cell, (v) => v < 0)) {
+        continue;
+      }
+      // of off the top
+      if (cell[0] > limits[0] + 1) {
+        continue;
+      }
+      if (cell[1] > limits[1] + 1) {
+        continue;
+      }
+      if (cell[2] > limits[2] + 1) {
+        continue;
+      }
+
+      const neighbor = _.get(grid, cell);
+      // if the neighbor exists, mark it as being steamed
+      // otherwise it's another empty cell to investigate
+      if (neighbor) {
+        ++neighbor.steamed;
+      } else {
+        open.push(cell);
+      }
+    }
+  }
+
+  return _(grid).flatten().flatten().compact().map("steamed").sum();
 }
--- src/day18.spec.js
+++ src/day18.spec.js
@@ -37,10 +37,11 @@ describe("day18", () => {
   describe("part 2", () => {
     it("should work with the sample", () => {
       const actual = part2(exampleInput);
-      expect(actual).toStrictEqual();
+      expect(actual).toStrictEqual(58);
     });
     it("should work with the puzzle input", () => {
       const actual = part2(puzzleInput);
+      expect(actual).toBeGreaterThan(2506);
       expect(actual).toStrictEqual();
     });
   });

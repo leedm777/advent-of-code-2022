fb71c31f2038331d1e16bef91dbf575549c8793b
David M. Lee <leedm777@yahoo.com>
2022-12-24 11:21:22 -0600

    Day 22, skipping part 2
    For now, at least. Need to think about how to model squares on the
    surface of a cube :-/

--- src/day22.spec.js
+++ src/day22.spec.js
@@ -31,7 +31,7 @@ describe("day22", () => {
     });
   });
 
-  describe("part 2", () => {
+  describe.skip("part 2", () => {
     it("should work with the sample", () => {
       const actual = part2(exampleInput);
       expect(actual).toStrictEqual();
